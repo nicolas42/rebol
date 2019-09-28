248b2f8650c718b393f23571790be62f7a2c2454
Nicolas Schmidt <nicolas42@gmail.com>
2019-09-29 03:04:25 +1000

    formatted code, added keyboard sniffer

--- gif-viewer/gif-viewer.r
+++ gif-viewer/gif-viewer.r
@@ -6,19 +6,25 @@ view-gif: func [ a ] [
 
 	draw-next: does [ a: next a i1/image: a/1 show i1 ]
 	draw-prev: does [ a: back a i1/image: a/1 show i1 ]
-
+	load-gif: does [ files: request-file a: load files/1 i1/size: a/1/size i1/image: a/1 show i1 ]
+	zoom-in: [ i1/size: i1/size * 1.1 show i1 ]
+	zoom-out: [ i1/size: i1/size / 1.1 show i1 ]
 	win: layout [
 
 	backcolor white
 	across
-		button "load" keycode [#"^M"] [ files: request-file a: load files/1 i1/size: a/1/size i1/image: a/1 show i1 ] 
-		button "prev" keycode [left #"j"] [ draw-prev ] 
-		button "next" keycode [right #"l"] [ draw-next ]
-		button "+" keycode [#"="] [ i1/size: i1/size * 1.2 show i1 ] 
-		button "-" keycode [#"-"] [ i1/size: i1/size / 1.2 show i1 ] 
+		button "load" 	keycode [#"^M"] 		[ load-gif ] 
+		button "prev" 	keycode [left #"j"] 	[ draw-prev ] 
+		button "next" 	keycode [right #"l"] 	[ draw-next ]
+		button "+" 		keycode [#"="] 			[ zoom-in ] 
+		button "-" 		keycode [#"-"] 			[ zoom-out ] 
 		
-		button "quit" keycode [#"^[" #"q"] [ unview ] return
-	    i1: image a/1 [ draw-next ]
+		button "quit" keycode [#"^[" #"q" #"^q"] [ unview ] return
+	    i1: image a/1 [ draw-next ] feel [ engage: func [ face action event ] [
+			probe event/key
+			event
+		] ]
+				
 	]
 	
 	view/new/options win [ resize ] 

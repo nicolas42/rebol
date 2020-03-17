79f0415d9bab5d6c1814f576b1baf2d0a9b1884a
nicolas42 <nicolas42@gmail.com>
2020-03-17 11:39:38 +1000

    Update nit-version-control.r

--- nit-version-control.r
+++ nit-version-control.r
@@ -1,3 +1,14 @@
+rebol []
+
+comment { 
+	In the future perhaps now-descending should use the gmt time for a team who 
+	might move around.
+}
+
+make-dir %.nit/files/
+make-dir %.nit/versions/
+
+
 file-walk: func [
 	{Deep read a directory given a dir d
 	an output block o and a boolean function fn}
@@ -8,23 +19,55 @@ file-walk: func [
 	foreach f f [if #"/" = last f [file-walk d/:f :fn]]
 ]
 
+now-descending: does [
+	now-time: replace/all mold now/time ":" ""
+	rejoin [ "" now/year now/month now/day "-" now-time ]
+	
+]
 
-; Save
-v: copy []  
-file-walk %. func[f][ 
-filename: join %.nit/files/ mold checksum/method f 'sha1
-replace/all filename "#" ""
-replace/all filename "{" ""
-replace/all filename "}" ""
-print filename
+save: funct[] [
 
-write filename read f
-append v filename  append v f   
-]   
-write/lines %.nit/versions/20200317.txt v
+	v: copy [] 
+	file-walk %. func[f][ 
+	
+		if find/match f "./.nit" [ return ]
+		
+		filename: join %./.nit/files/ mold checksum/method f 'sha1
+		foreach c "#{}" [replace/all filename c "" ]
+		
+		write filename read f
+		append v filename  append v f   
+	]   
+	write/lines rejoin [ %.nit/versions/ now-descending ".txt" ] v
+	write/lines %.nit/versions/current.txt v
+
+
+	forskip v 2 [ print rejoin [ v/2 " => " v/1 ] ]
+]
+
+
+restore: funct [/file version-file] [
+
+
+comment {
+	d: %.nit/versions/
+	v: read d
+	if n < 0 [ n: add n length? v ]
+	f: read/lines join d v/:n
+}	
+	
+	f: read/lines %.nit/versions/current.txt
+	if file [
+		f: read/lines version-file
+	]
+
+	foreach f read %. [ if not find/match f ".nit" [ either dir? f [ delete-dir f ] [ delete f ] ] ]
+	
+	forskip f 2 [ print rejoin [ f/1 " => " f/2 ] attempt [ write to-file f/2 read to-file f/1 ] ]
+	
+	
+	
+]
 
 
 
-; Restore
-f: read/lines %.nit/versions/20200317.txt
-forskip f 2 [ attempt [ write to-file f/2 read to-file f/1 ] ]
